1154f1916fbd8e9babe500101c2912e199f8f2c6
BuildTools <unconfigured@null.spigotmc.org>
2022-01-10 09:25:00 +0700

    add project to git

---
+++ src/main/java/tpawithgui/rtree.java
@@ -0,0 +1,17 @@
+package tpawithgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class rtree extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
